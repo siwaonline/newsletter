c015a22f318f59f414a05f2d63d52b7085e3d052
geraldaistleitner <gerald.aistleitner@gmail.com>
2019-02-27 08:21:26 +0100

    Raise Compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Newsletter',
     'description' => 'Send any pages as Newsletter and provide statistics on opened emails and clicked links.',
     'category' => 'module',
-    'version' => '4.0.0',
+    'version' => '5.0.0-dev',
     'state' => 'stable',
     'uploadfolder' => 1,
     'author' => 'Ecodev',
@@ -12,9 +12,9 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Ecodev',
     'constraints' => [
         'depends' => [
-            'php' => '5.6.0-0.0.0',
-            'typo3' => '7.6.0-8.99.99',
-            'scheduler' => '7.6.0-8.99.99',
+            'php' => '7.2.0-0.0.0',
+            'typo3' => '9.5.0-10.x',
+            'scheduler' => '9.5.0-10.x',
         ],
     ],
 ];
