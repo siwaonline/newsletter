3c1eea2773b886e64392f6f0ff1adab9995477e7
Ayke Halder <rr-it@users.noreply.github.com>
2020-04-13 21:35:30 +0200

    Bounce mail handling via Symfony CLI and composer CLI ready (#1)
    * Bounce mail handling via Symfony Console Commands (cli)
    
    * Remove bounce mail handling via old cliKeys
    
    * Switch to text instead of success message
    
    * Configurable path to TYPO3 cli and PHP binary for cli call by fetchmail
    
    * [Cleanup] remove unnecessary FQCN
    
    * Switch fetchmail to new cli command newsletter:mdabounce

---
+++ Classes/Command/MdaBounceCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Ecodev\Newsletter\Command;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Newsletter function for handling bounce mails
+ */
+class MdaBounceCommand extends Command
+{
+    /**
+     * Configure the command by defining the name, options and arguments
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Bounce mail handling MDA for fetchmail')
+            ->addArgument(
+                'rawmailsource',
+                InputArgument::REQUIRED,
+                'Raw mail source to be piped from fetchmail'
+            );
+    }
+
+    /**
+     * Executes the command for handling the mail source
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title($this->getDescription());
+        $output = 'Handling raw mail source.';
+
+        // Read piped mail raw source
+        $content = $input->getArgument('rawmailsource');
+
+        // Dispatch it to analyze its bounce level an take appropriate action
+        $bounceHandler = GeneralUtility::makeInstance(
+            'Ecodev\\Newsletter\\BounceHandler',
+            $content
+        );
+        $bounceHandler->dispatch();
+
+        $io->text($output);
+    }
+}
